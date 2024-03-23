0e2d63e772fe00e486b79b4b7c5e8f6e6d229230
BaldPolnareff <baldpolnareff@gmail.com>
2024-03-23 22:09:18 +0100

    added a simple div to use for interaction with the intersection observer API

---
+++ src/components/Observer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="observer"></div>
+</template>
+
+<style scoped>
+.observer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20px;
+}
+</style>
